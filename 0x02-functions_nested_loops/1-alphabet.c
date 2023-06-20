afdefec31788d1cd8aa0efa7d77e61c3e24adf74
Brafafabra <fnkyungu@gmail.com>
2023-06-20 13:19:05 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,8 +3,7 @@
 /**
  * main - Entry point of the program
  *
- * Description: This function calls the print_alphabet function to print
- *              the alphabet in lowercase followed by a new line.
+ * Description: This function print the alphabet in lowercase followed by a new line.
  *
  * Return: Always 0 (indicating success)
  */
@@ -18,7 +17,7 @@ return (0);
  * print_alphabet - Prints the alphabet in lowercase
  *
  * Description: This function prints the lowercase alphabet followed by a new line.
- *              It uses the _putchar function to print each letter individually.
+ *              
  */
 void print_alphabet(void)
 {
