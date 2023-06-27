d119399c295f0c738f281df7b6cc64921109e255
Brafafabra <fnkyungu@gmail.com>
2023-06-27 15:51:24 -0700

    five

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -9,11 +9,11 @@ void reset_to_98(int *n);
  */
 int main(void)
 {
-	int n = 402;
+    int n = 402;
 
-	printf("n = %d\n", n);
-	reset_to_98(&n);
-	printf("n = %d\n", n);
+    printf("n = %d\n", n);
+    reset_to_98(&n);
+    printf("n = %d\n", n);
 
-	return (0);
+    return 0;
 }
--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,21 +6,5 @@
  */
 void reset_to_98(int *n)
 {
-	*n = 98;
-}
-
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-	int n = 402;
-
-	printf("n = %d\n", n);
-	reset_to_98(&n);
-	printf("n = %d\n", n);
-
-	return (0);
+    *n = 98;
 }
