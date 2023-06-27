a28a8c9caad2e3ac5834afd9b41be3fd327f4d29
Brafafabra <fnkyungu@gmail.com>
2023-06-27 15:47:37 -0700

    five

---
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void reset_to_98(int *n);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 402;
+
+	printf("n = %d\n", n);
+	reset_to_98(&n);
+	printf("n = %d\n", n);
+
+	return (0);
+}
