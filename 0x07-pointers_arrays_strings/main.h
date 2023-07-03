9332090b85fa14355cb50f33a11b94c3e874cbeb
Brafafabra <fnkyungu@gmail.com>
2023-07-03 12:46:33 -0700

    seven

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
 char *_memset(char *s, char b, unsigned int n);
 void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
+void print_chessboard(char (*a)[8]);
+void print_chessboard(char (*a)[8]);
