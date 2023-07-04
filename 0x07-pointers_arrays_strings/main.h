bdfc482a0285638e10cb8fbff175e695770d68ad
Brafafabra <fnkyungu@gmail.com>
2023-07-03 23:54:54 -0700

    main

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 
 unsigned int _strspn(char *s, char *accept);
 char *_memset(char *s, char b, unsigned int n);
@@ -7,4 +9,5 @@ void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 char *_strchr(char *s, char c);
-char *_strchr(char *s, char c);
+
+#endif /* MAIN_H */
