bd0c7c8f28e84f7bea07a30a4b16c468509bab7c
Brafafabra <fnkyungu@gmail.com>
2023-07-05 07:16:42 -0700

    eight

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - Dummy implementation of putchar
+ * @c: The character to be printed
+ *
+ * Return: Always 0
+ */
+int _putchar(char c)
+{
+	putchar(c);
+	return (0);
+}
