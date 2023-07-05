3f9549e5b3a44ca16f39606cbaf5961bc0843102
Brafafabra <fnkyungu@gmail.com>
2023-07-05 07:38:02 -0700

    eight

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string using recursion
+ * _puts_recursion - Prints a string followed by a new line
  * @s: The string to be printed
- *
- * Description: This function recursively prints a string followed by a new line.
  */
 void _puts_recursion(char *s)
 {
