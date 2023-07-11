1f51d37b1ccab1c169fcb124fd01335e62875f47
Brafafabra <fnkyungu@gmail.com>
2023-07-11 12:44:07 -0700

    wer

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,5 +6,7 @@
 
 char *create_array(unsigned int size, char c);
 void simple_print_buffer(char *buffer, unsigned int size);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
 
 #endif /* MAIN_H */
