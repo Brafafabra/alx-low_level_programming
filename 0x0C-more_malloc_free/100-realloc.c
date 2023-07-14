9e80351f9613decada34f9bc14b0618489b1317d
Brafafabra <fnkyungu@gmail.com>
2023-07-14 01:22:35 -0700

    fe

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,37 +11,37 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-    void *new_ptr;
-    unsigned int i;
+	void *new_ptr;
+	unsigned int i;
 
-    if (new_size == old_size)
-        return (ptr);
+	if (new_size == old_size)
+		return (ptr);
 
-    if (ptr == NULL)
-    {
-        new_ptr = malloc(new_size);
-        if (new_ptr == NULL)
-            return (NULL);
-        return (new_ptr);
-    }
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return (NULL);
+		return (new_ptr);
+	}
 
-    if (new_size == 0)
-    {
-        free(ptr);
-        return (NULL);
-    }
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
-    new_ptr = malloc(new_size);
-    if (new_ptr == NULL)
-        return (NULL);
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
 
-    if (new_size > old_size)
-        new_size = old_size;
+	if (new_size > old_size)
+		new_size = old_size;
 
-    for (i = 0; i < new_size; i++)
-        *((char *)new_ptr + i) = *((char *)ptr + i);
+	for (i = 0; i < new_size; i++)
+		*((char *)new_ptr + i) = *((char *)ptr + i);
 
-    free(ptr);
+	free(ptr);
 
-    return (new_ptr);
+	return (new_ptr);
 }
