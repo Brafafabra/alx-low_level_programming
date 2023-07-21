cd01e12fb735af32a25b32f51c1fdab71950ff84
Brafafabra <fnkyungu@gmail.com>
2023-07-21 14:16:37 -0700

    final

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,19 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
+/**
+ * struct print_func - Struct for printing functions
+ * @type: The type of argument
+ * @func: The corresponding printing function
+ */
+typedef struct print_func
+{
+	char type;
+	void (*func)(va_list);
+} print_func_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_all(const char * const format, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
