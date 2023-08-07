d7da501be2041bc5882a9e3184856d7f5f130799
Brafafabra <fnkyungu@gmail.com>
2023-08-07 13:03:16 -0700

    message

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends text to the end of a file
+ * @filename: Name of the file
+ * @text_content: Text to append to the file
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, text_len, write_status;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		text_len = 0;
+		while (text_content[text_len] != '\0')
+			text_len++;
+
+		write_status = write(fd, text_content, text_len);
+		if (write_status == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
