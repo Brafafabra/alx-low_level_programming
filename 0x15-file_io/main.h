d0916a3a25b971bfe4ddb44c58489e43ec7087e6
Brafafabra <fnkyungu@gmail.com>
2023-08-07 13:10:31 -0700

    comm

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,31 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#define EI_NIDENT 16
+
+typedef struct
+{
+	unsigned char e_ident[EI_NIDENT];
+	uint16_t e_type;
+	uint16_t e_machine;
+	uint32_t e_version;
+	uint64_t e_entry;
+	uint64_t e_phoff;
+	uint64_t e_shoff;
+	uint32_t e_flags;
+	uint16_t e_ehsize;
+	uint16_t e_phentsize;
+	uint16_t e_phnum;
+	uint16_t e_shentsize;
+	uint16_t e_shnum;
+	uint16_t e_shstrndx;
+} Elf64_Ehdr;
+
+#define ELFCLASS32 1
+#define ELFCLASS64 2
+
+#define EI_CLASS 4
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
